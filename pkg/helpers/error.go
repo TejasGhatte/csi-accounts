b9a13760e9b773d8b00b2d2073748f81533cd81f
TejasGhate <tejasghatte5@gmail.com>
2024-12-16 20:18:51 +0530

    feat: added routers and helpers

---
+++ pkg/helpers/error.go
@@ -0,0 +1,44 @@
+package helpers
+
+import (
+	"github.com/gofiber/fiber/v2"
+
+	"csi-accounts/pkg/config"
+)
+
+type AppError struct {
+	Code       int
+	Message    string
+	LogMessage string
+	Err        error
+}
+
+func (err AppError) Error() string {
+	return err.LogMessage
+}
+
+func ErrorHandler(c *fiber.Ctx, err error) error {
+	Code := 500
+	Message := config.SERVER_ERROR
+	Error  := err
+	if e, ok := err.(*fiber.Error); ok {
+		Code = e.Code
+		Message = e.Message
+	}
+	if e, ok := err.(*AppError); ok {
+		Code = e.Code
+		Message = e.Message
+		Error = e
+	}
+
+	if Message == config.DATABASE_ERROR {
+		go LogDatabaseError("Database Error", Error, c.Path())
+	} else if Code == 500 {
+		go LogServerError("Server Error", Error, c.Path())
+	}
+
+	return c.Status(Code).JSON(fiber.Map{
+		"status": "failed",
+		"message": Message,
+	})
+}
