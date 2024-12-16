1baa40c36f99994f3ca3bf8221a1b1e6ea187945
TejasGhate <tejasghatte5@gmail.com>
2024-12-16 20:20:18 +0530

    feat: modified main

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
-	"csi-accounts/config"
-	"log"
+	"csi-accounts/internal/initializers"
+	"csi-accounts/internal/routers"
+	"csi-accounts/pkg/helpers"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
+func init() {
+	initializers.LoadEnv()
+	initializers.ConnectToDB()
+	initializers.RunMigrations()
+	initializers.CustomZerologLogger()
+}
 func main() {
-	cfg := config.LoadConfig()
-	app := fiber.New()
-	log.Fatal(app.Listen(cfg.ServerPort))
+
+	app := fiber.New(fiber.Config{
+		ErrorHandler: helpers.ErrorHandler,
+	})
+	app.Use(logger.New())
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	routers.SetUp(app)
+
+	app.Listen(":" + initializers.CONFIG.PORT)
 }
