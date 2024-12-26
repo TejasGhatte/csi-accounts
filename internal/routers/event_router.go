e1099e95fd7303cbc12c4c2ff8341d2ddee2d34b
TejasGhate <tejasghatte5@gmail.com>
2024-12-26 14:07:47 +0530

    feat: added membership controllers

--- internal/routers/event_router.go
+++ internal/routers/event_router.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"csi-accounts/internal/controllers"
+	"csi-accounts/internal/middlewares"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -14,4 +15,8 @@ func EventRouter(app *fiber.App) {
 	eventRouter.Post("/", controllers.CreateEvent)
 	eventRouter.Patch("/:eventID", controllers.UpdateEvent)
 	eventRouter.Delete("/:eventID", controllers.DeleteEvent)
+
+	eventMembershipRouter := eventRouter.Group("/:eventID/memberships")
+	eventMembershipRouter.Get("/", controllers.GetEventMemberships)
+	eventMembershipRouter.Post("/coordinators/:userID", middlewares.EventAdminAuthorization, controllers.AddEventCoordinator)
 }
